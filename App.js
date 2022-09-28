a5a58fcc3c3d4c3b5be5eb33a6370516fb410f8c
Eviix <daniel.ristikari@gmail.com>
2022-09-28 10:44:25 +0300

    search works now

--- App.js
+++ App.js
@@ -1,7 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList, SafeAreaView, Dimensions, Alert } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
+  const [searchLocation, setSearchLocation] = useState('');
+  const [mapLatitude, setMapLatitude] = useState(60.200692);
+  const [mapLongitude, setMapLongitude] = useState(24.934302);
+  const [mapLatitudeDelta, setMapLatitudeDelta] = useState(0.0250);
+  const [mapLongitudeDelta, setMapLongitudeDelta] = useState(0.0250);
+
+  cconst [address, setAddress] = useState('');
+  const [markerLat, setMarkerLat] = useState(0.0);
+  const [markerLong, setMarkerLong] = useState(0.0);
+  const [regionLat, setRegionLat] = useState(0.0);
+  const [regionLong, setRegionLong] = useState(0.0);
+  const [data, setData] = ([]);
+
+
+  const fetchCoordinates = () => {
+    fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=s9RDVPqGhFHEQCfGvUtJNWgUDhmDfc9v&location=${address}`)
+    .then(response => response.json())
+    .then(data => {
+      const lat = data.results[0].locations[0].latLng.lat;
+      const long = data.results[0].locations[0].latLng.lng;
+      setMarkerLat(lat);
+      setRegionLat(lat);
+      setMarkerLong(long);
+      setRegionLong(long);
+    })
+    .catch(error => {
+      console.log(error)
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -18,3 +49,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
